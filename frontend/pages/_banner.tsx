5728a7b22861603da4c4afe2bb7dab0dbf9eb039
z33p <raphael00fellipe@gmail.com>
2021-07-10 20:51:39 -0300

    feat: added image to banner

--- frontend/pages/_banner.tsx
+++ frontend/pages/_banner.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface Props {
   style: React.CSSProperties;
 }
 
+interface Sizes {
+  width: number;
+  height: number;
+}
+
 export default function Banner({ style }: Props) {
+  const [sizes, setSizes] = useState<Sizes>();
+  const bannerId = "banner";
+
+  useEffect(() => {
+    const banner = document.getElementById(bannerId)?.getBoundingClientRect();
+
+    if (banner) {
+      let { width, height } = banner;
+
+      if (width > height) {
+        width = height;
+      }
+
+      setSizes({
+        width: width,
+        height: height,
+      });
+    }
+  }, []);
 
   return (
     <section
@@ -12,6 +37,7 @@ export default function Banner({ style }: Props) {
       style={style}
     >
       <div
+        id={bannerId}
         style={{
           position: "absolute",
           backgroundColor: "rgba(0, 0, 0, 0.45)",
@@ -22,12 +48,28 @@ export default function Banner({ style }: Props) {
         className="row align-items-center"
       >
         <div className="text-center align-middle text-white">
-          <h1 className="display-3">RAPHAEL FELLIPE</h1>
-          <p className="h4">
-            .NET Developer {"&&"} MSSQL | Flutter {"&&"} React Enthusiast
-          </p>
+          {!sizes ? (
+            <div className="spinner-border text-info" />
+          ) : (
+            <div className="">
+              <h1 className="display-3">RAPHAEL FELLIPE</h1>
+              <p className="h4">
+                .NET Developer {"&&"} MSSQL | Flutter {"&&"} React Enthusiast
+              </p>
+            </div>
+          )}
         </div>
       </div>
+
+      {!sizes ? undefined : (
+        <Image
+          src="/me.jpg"
+          alt="me"
+          width={sizes.width}
+          height={sizes.height}
+          quality="100"
+        />
+      )}
     </section>
   );
 }

0797837e76faf71b4b22c857ec87d155d3f7c302
z33p <raphael00fellipe@gmail.com>
2021-08-05 11:21:26 -0300

    feat: routing pinned_repositories

--- serverless/GitHubSyncer/GitHubSyncer/Contracts/AppRoutes.cs
+++ serverless/GitHubSyncer/GitHubSyncer/Contracts/AppRoutes.cs
@@ -4,13 +4,13 @@ namespace GithubSyncer.Contracts
     public static class AppRoutes
     {
         // Base
-        public const string BASE_URL = "githubsyncer";
+        public const string BASE_URL = "~/githubsyncer";
 
         public static class FilesController
         {
-            public const string Root = AppRoutes.BASE_URL + "/Files";
+            public const string Root = AppRoutes.BASE_URL + "/files";
 
-            public const string PinnedRepositories = Root + "/PinnedRepositories";
+            public const string PinnedRepositories = "pinned_repositories";
         }
     }
 }

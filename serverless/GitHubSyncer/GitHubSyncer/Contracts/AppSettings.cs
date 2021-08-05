69c107166bdc46f740f73364a91983048831fff1
z33p <raphael00fellipe@gmail.com>
2021-08-04 21:38:09 -0300

    feat: s3 husky bucket appsettings reference

--- serverless/GitHubSyncer/GitHubSyncer/Contracts/AppSettings.cs
+++ serverless/GitHubSyncer/GitHubSyncer/Contracts/AppSettings.cs
@@ -3,6 +3,13 @@ namespace GitHubSyncer.Contracts
     public class AppSettings
     {
         public GitHubSettings GitHub { get; set; }
+        public S3Buckets Buckets { get; set; }
+
+        public class S3Buckets
+        {
+            public string Husky { get; set; }
+
+        }
 
         public class GitHubSettings
         {
